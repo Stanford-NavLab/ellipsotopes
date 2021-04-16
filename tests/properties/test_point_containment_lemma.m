fbddbe849c5d19ee2a6a6c2522d536507ae709e0
Shreyas Kousik <shreyas.kousik@gmail.com>
2021-04-16 08:04:04 -0700

    fixed point containment lemma gradient
    but it'll be faster to write our own solver I bet

--- tests/properties/test_point_containment_lemma.m
+++ tests/properties/test_point_containment_lemma.m
@@ -2,8 +2,8 @@
 % This script tests the ellipsotope point containment lemma.
 %
 % Authors: Shreyas Kousik
-% Created: 15 Apr 2021 (just barely, it's like 11:49 pm lol)
-% Updated: not yet
+% Created: 15 Apr 2021
+% Updated: 16 Apr 2021
 clear ; clc
 %% user parameters
 % rng seed
@@ -11,16 +11,18 @@ rng(0)
 
 % point to test
 p_test = [1.0 ; 1.0] ; % this point is inside
-% p_test = [1.5; 0.0] ; % this point is inside
-
+% p_test = [2.0; 0.0] ; % this point is inside
 
 % ellipsotope
-p_norm = 4 ;
+p_norm = 2 ;
 c = zeros(2,1) ;
-G = rand(2,3) ;
+G = 2*rand(2,4) - 1 ;
 A = [] ;
 b = [] ;
-I = {1:2,3} ;
+I = {1:2,3,4} ;
+
+% whether or not to time it
+flag_time_check = false ;
 
 %% automated from here
 % get sizes of things
@@ -31,22 +33,28 @@ n_con = size(A,1) ;
 A_eq = [G ; A] ;
 b_eq = [p_test - c ; b] ;
 
-% set up program
+% set up program cost
 cost = @(x) point_cointainment_cost(x,p_norm,I) ;
-x_0 = rand(n_gen,1) ;
-options = optimoptions('fmincon','Display','off','CheckGradients',true) ;
+x_0 = pinv(A_eq)*b_eq ;
+options = optimoptions('fmincon','Display','off',...
+    'CheckGradients',true,...
+    'SpecifyObjectiveGradient',true) ;
 [x_opt,f_val] = fmincon(cost,x_0,[],[],A_eq,b_eq,[],[],[],options) ;
 
 if f_val <= 1
-    disp('Test point is INSIDE the ellipsotope!')
+    disp('Test point is INSIDE the ellipsotope')
+elseif f_val > 1
+    disp('Test point is OUTSIDE the ellipsotope')
 else
-    disp('Test point is OUTSIDE the ellipsotope!')
+    disp('Test point is ON THE BOUNDARY of the ellipsotope')
 end
 
 % time the containment check
-avg_time = timeit(@() fmincon(cost,x_0,[],[],A_eq,b_eq,[],[],[],options)) ;
-disp(['It takes ',num2str(avg_time,'%0.4f'),' s to compute the point containment.'])
-
+if flag_time_check
+    options = optimoptions('fmincon','Display','off','SpecifyObjectiveGradient',true) ;
+    avg_time = timeit(@() fmincon(cost,x_0,[],[],A_eq,b_eq,[],[],[],options)) ;
+    disp(['It takes ',num2str(avg_time,'%0.4f'),' s to compute the point containment.'])
+end
 %% plotting
 figure(1) ; clf ; axis equal ; hold on ; grid on ;
 
@@ -65,15 +73,15 @@ set(gca,'fontsize',15)
 function [c,gc] = point_cointainment_cost(x,p_norm,I)
     n_I = length(I) ;
     x_vals = nan(n_I,1) ;
-    
+
     for idx = 1:n_I
-        x_vals(idx) = vecnorm(x(I{idx}),p_norm) ;
+        x_vals(idx) = sum(x(I{idx}).^p_norm) ;
     end
-    
+
     % compute cost
     [c,c_idx] = max(x_vals) ;
-    
-    % compute cost gradient
+
+    % compute cost (sub)gradient
     gc = zeros(1,length(x)) ;
-    gc(I{c_idx}) = p_norm*vecnorm(x(I{c_idx}),p_norm)*(x(I{c_idx}).^(p_norm-1)) ;
+    gc(I{c_idx}) = p_norm*(x(I{c_idx}).^(p_norm-1)) ;
 end

b9c517c898a565cd0c42602e632f3d55352b02bc
Shreyas Kousik <shreyas.kousik@gmail.com>
2021-05-20 18:18:04 -0700

    Update test_sorting_ellipsoids.m

--- tests/properties/test_sorting_ellipsoids.m
+++ tests/properties/test_sorting_ellipsoids.m
@@ -1,59 +1,83 @@
 %% description
-% This script sorts ellipsoids by their volume and similarity of their
-% "covariance" matrices
+% This script sorts ellipsoids by the similarity metrics in [1]
+%
+% [1] Moshtaghi, M., Havens, T.C., Bezdek, J.C., Park, L., Leckie, C.,
+%     Rajasegarar, S., Keller, J.M. and Palaniswami, M., 2011. Clustering
+%     ellipses for anomaly detection. Pattern Recognition, 44(1), pp.55-69.
 %
 % Authors: Shreyas Kousik
 % Created: 19 May 2021
-% Updated: nah
+% Updated: 20 May 2021
 clear ; clc ;
 %% user parameters
 % rng seed
-rng(0) ; 
+% rng(100) ; 
 
 % number of ellipsoids to consider
-n_ell = 4 ;
+n_ell = 3 ;
 
 %% automated from here
-% randomly generate a bunch of ellipsoids and compute their volumes
+% randomly generate a bunch of ellipsoids
 E_list = cell(1,n_ell) ;
-v_list = nan(1,n_ell) ;
-for idx = 1:n_ell
-    E_list{idx} = rand_range(1,10).*make_random_covariance_matrix(2) ;
-    v_list(idx) = vol_ellipsoid(E_list{idx}) ;
-end
-
-v_ratios = v_list./v_list(1) ;
-
-%% compute ellipsoid similarities
-d_list = nan(1,n_ell) ;
+R_list = cell(1,n_ell) ;
+l_list = cell(1,n_ell) ;
 
 for idx = 1:n_ell
-    d_list(idx) = KL_ellipsoid(E_list{1},E_list{idx}) ;
+    Q_idx = make_random_covariance_matrix(2) ;
+    
+    [R_idx, l_idx] = eig(Q_idx) ;
+    E_list{idx} = Q_idx ;
+    R_list{idx} = R_idx ;
+    l_list{idx} = diag(l_idx) ;
 end
 
-% d_list = d_list./v_ratios ;
+%% compute similarities to first ellipsoid
+R_1 = R_list{1} ;
+l_1 = l_list{1} ;
 
-d_list(1) = 0 ;
-d_max = max(d_list) ;
+sim_list = ones(1,n_ell) ;
+
+for idx = 1:n_ell
+%     % compute orientation similarity
+    R_idx = R_list{idx} ;
+    th_1 = acos(diag(R_1'*R_idx)) ;
+    th_2 = acos(diag(R_idx'*R_1)) ;
+    val = min(vecnorm(sin([th_1,th_2]),2)) ;
+    sim_idx = exp(-val) ;
+    sim_list(idx) = sim_idx ;
+end
+
+%% compute similarities based on longest semi-axis
+G_1 = get_generators(E_list{1}) ;
+
+%% sort by similarity
+[sim_list,idxs] = sort(sim_list,'descend') ;
+E_list = E_list(idxs) ;
+
+%% plotting setup
+th_vec = linspace(0,2*pi,200) ;
+C = [cos(th_vec) ; sin(th_vec)] ;
 
 %% plotting
 figure(1) ; clf ; axis equal ; hold on ; grid on ;
 
 for idx = 1:n_ell
-    E = ellipsoid(E_list{idx}) ;
+    % transform unit ball
+    G = get_generators(E_list{idx}) ;
+    P = G*C + [idx-1;0];
     
     % plot color based on d_list
     if idx > 1
-        r = d_list(idx)./d_max ;
-        c = [0 r r] ;
+        r = sim_list(idx) ;
+        c = [(1-r) 0 r] ;
     else
-        c = [0 1 0] ;
+        c = [0 0 1] ;
     end
-    h = plot(E) ;
-    h.Color = c ;
+    patch('faces',[1:200,1],'vertices',P','facecolor',c,'facealpha',1,...
+        'edgealpha',0)
 end
 
-set_plot_linewidths(1.5) 
+set_plot_linewidths(2) 
 
 %% helper functions
 function S = make_random_covariance_matrix(d)
@@ -68,13 +92,6 @@ function S = make_random_covariance_matrix(d)
     S = Q*D*Q' ;
 end
 
-function v = vol_ellipsoid(Q)
-    n = size(Q,1) ;
-    v_n = (pi^(n/2))/gamma(n/2 + 1) ;
-    v = v_n.*(det(inv(Q))^(1/2)) ;
-end
-
-function d = KL_ellipsoid(Q_1,Q_2)
-    k = size(Q_1,1) ;
-    d = (1/2)*(trace(inv(Q_1)*Q_2) - k + log(det(Q_1)/det(Q_2))) ;
+function G = get_generators(Q)
+    G = inv(inv(Q)^(1/2)) ;
 end

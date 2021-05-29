1bba7948866b2f4f39818881233b4e3df0265dd5
Shreyas Kousik <shreyas.kousik@gmail.com>
2021-05-29 15:10:18 -0700

    Update test_constraint_and_generator_elim.m
    things are borked :(

--- tests/properties/test_constraint_and_generator_elim.m
+++ tests/properties/test_constraint_and_generator_elim.m
@@ -26,7 +26,7 @@ j_rdc = 3 ;
 % make random etope
 [E,c,G,A,b,I] = make_random_ellipsotope(p_norm,n_dim,n_gen,n_con) ;
 
-vecnorm(G)
+vecnorm(A(1,:)./G)
 
 % solve the first constraint for the coefficient value
 E_j1 = zeros(n_gen,n_con) ;
@@ -70,6 +70,54 @@ end
 
 E_rdc = ellipsotope(p_norm,c_rdc,G_rdc,A_rdc,b_rdc,I_rdc) ;
 
+%% compute bounds on ball product intersecting affine subspace
+% initialize
+int_E = [-ones(n_gen,1), ones(n_gen,1)] ;
+int_R = [-inf(n_gen,1), inf(n_gen,1)] ;
+
+% iterate Algorithm 1 of [1] NOTE! THIS IS BROKEN RIGHT NOW FOR SOME REASON
+for idx_i = 1:n_con
+    for idx_j = 1:n_gen
+        a_ij = A(idx_i,idx_j) ;
+        
+        if a_ij ~= 0
+            R_j = int_R(idx_j,:) ;
+            
+            a_ij_inv = 1./a_ij ;
+            
+            idxs_k = 1:n_gen ;
+            idxs_k(idx_j) = [] ;
+                        
+            R_j_RHS = a_ij_inv*b(idx_i).*[1 1] ;
+            for idx_k = idxs_k
+                a_ik = A(idx_i,idx_k) ;
+                E_k = int_E(idx_k,:) ;
+                R_j_RHS = interval_subtract(R_j_RHS, a_ij_inv.*a_ik.*E_k) ;
+            end
+            
+            int_R(idx_j,:) = interval_intersect(R_j, R_j_RHS) ;
+            
+            int_E(idx_j,:) = interval_intersect(int_E(idx_j,:),R_j) ;
+        end
+    end
+end
+
+% %% test computing approximate Hausdorff distance
+% % this applies the method in the appendix of [1]
+% 
+% % compute Q submatrix
+% Q = G'*G + eye(n_gen) ;
+% Qinv = inv(Q) ;
+% 
+% % compute H_hat approx for each j
+% for j_rdc = 1:n_gen
+%     % create j unit vector
+%     e_j = zeros(n_gen + n_gcon,1) ;
+%     e_j(j_rdc) = 1 ;
+%     
+%     % create matrix
+% end
+
 %% plotting
 figure(1) ; clf ; axis equal ; hold on ; grid on ;
 
@@ -78,3 +126,19 @@ plot(E_rdc,'facecolor','r','edgecolor','r','linestyle','--','facealpha',0.1)
 
 % plot origetope
 plot(E) ;
+
+%% helper functions
+function I = interval_intersect(I_1,I_2)
+     I_lo = [I_1(:,1), I_2(:,1)] ;
+     I_hi = [I_1(:,2), I_2(:,2)] ;
+     
+     I = [max(I_lo,[],2), min(I_hi,[],2)] ;
+end
+
+function I = interval_add(I_1,I_2)
+    I = [I_1(:,1) + I_2(:,1), I_1(:,2) + I_2(:,2)] ;
+end
+
+function I = interval_subtract(I_1,I_2)
+    I = [I_1(:,1) - I_2(:,2), I_1(:,2) - I_2(:,1)] ;
+end

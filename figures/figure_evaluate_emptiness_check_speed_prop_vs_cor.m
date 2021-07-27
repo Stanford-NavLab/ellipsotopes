a5a940e985f77697d42977ee8983c9621a050191
Shreyas Kousik <shreyas.kousik@gmail.com>
2021-07-27 10:06:10 -0700

    :( fixed things but made them slower

--- figures/figure_evaluate_emptiness_check_speed_prop_vs_cor.m
+++ figures/figure_evaluate_emptiness_check_speed_prop_vs_cor.m
@@ -1,12 +1,13 @@
 %% description
 % This script evaluates the emptiness check for ellipsotopes as a function
-% of the number of generators.
+% of the number of generators. We check the proposition versus the
+% corollary to see which is faster.
 %
 % NOTE this takes about 25 s to run on an i7 8-core processor.
 %
 % Authors: Shreyas Kousik
-% Created: 28 May 2021
-% Updated: 27 Jul 2021
+% Created: 27 Jul 2021
+% Updated: -- 
 clear ; clc
 %% user parameters
 % rng seed
@@ -18,14 +19,14 @@ n_dim = 2 ;
 n_con = 1 ;
 
 % range of properties to test
-n_gen_list = 1:20 ; % 1:10:101 ; % default is 1:20
-n_etopes_per_n_gen = 10 ; % default is 10
+n_gen_list = 1:10:101 ; % default is 1:20
+n_etopes_per_n_gen = 5 ; % default is 10
 
 %% automated from here
 n_n_gen = length(n_gen_list) ;
 
 % set up to store times
-t_avg_full = nan(n_etopes_per_n_gen,n_n_gen) ; % for nonempty etopes
+t_avg_prop = nan(n_etopes_per_n_gen,n_n_gen) ; % for nonempty etopes
 t_avg_empty = nan(n_etopes_per_n_gen,n_n_gen) ; % for empty etopes
 
 %% time etope emptiness check
@@ -38,27 +39,11 @@ for idx_gen = 1:n_n_gen
     for idx_tope = 1:n_etopes_per_n_gen
         % make a random ellipsotope
         [E,c,G,A,~,I] = make_random_ellipsotope(p_norm,n_dim,n_gen,n_con) ;
+        E.constraint_b = 10*n_gen.*rand(n_con,1) ;
 
-        %% time for nonempty etope
-        % set b for nonempty etopes
-        E.constraint_b = zeros(n_con,1) ;
-
-        % run emptiness check
-        t_avg = timeit(@() E.isempty(false,'feasibility')) ;
-        
-        t_avg_full(idx_tope,idx_gen) = t_avg ;
-        
-        %% time for empty etope
-        % set b for empty etopes
-        E.constraint_b = (2*n_gen).*ones(n_con,1) ;
-        
-        % % check that it's empty, yikes
-        % assert(E.isempty(),'Something went wrong! Ellipsotope is nonempty')
-
-        % run emptiness check
-        t_avg = timeit(@() E.isempty(false,'feasibility')) ;
-        
-        t_avg_empty(idx_tope,idx_gen) = t_avg ;
+        %% run emptiness checks
+        t_avg_prop(idx_tope,idx_gen) = timeit(@() E.isempty(true,'standard')) ;
+        t_avg_empty(idx_tope,idx_gen) = timeit(@() E.isempty(true,'feasibility')) ;
     end
     toc(start_tic_gen)
 end
@@ -69,7 +54,7 @@ disp([newline,'Total time elapsed:',newline,num2str(toc(start_tic_all),'%0.1f'),
 fh = figure(1) ; clf ;
 
 subplot(2,1,1) ; hold on ; grid on ;
-h_full = boxplot(t_avg_full,'Colors','b','labels',n_gen_list,'labelorientation','horizontal') ;
+h_prop = boxplot(t_avg_prop,'Colors','b','labels',n_gen_list,'labelorientation','horizontal') ;
 axis 'auto y'
 
 n = findobj(gcf,'tag','Outliers');
@@ -83,7 +68,7 @@ set_plot_linewidths(1.5) ;
 set_plot_fontsize(15) ;
 
 subplot(2,1,2) ; hold on ; grid on ;
-h_empty = boxplot(t_avg_empty,'Colors','r','labels',n_gen_list,'labelorientation','horizontal') ;
+h_cor = boxplot(t_avg_empty,'Colors','r','labels',n_gen_list,'labelorientation','horizontal') ;
 axis 'auto y'
 
 % legend([h_full,h_empty],{'full','empty'},'location','northwest')
@@ -92,4 +77,4 @@ ylabel('time [s]')
 set_plot_linewidths(1.5) ;
 set_plot_fontsize(15) ;
 
-save_figure_to_png(fh,'emptiness_check_time.png')
+save_figure_to_png(fh,'emptiness_check_time_prop_vs_cor.png')
